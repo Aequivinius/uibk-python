bcd8afd9c77dc4545caa348fe41953e4e1bb3f80
nvdh <nsjvdh@gmail.com>
2021-03-18 11:20:17 +0000

    van den Hoeven: exercise 3

--- exercise-3/palindrome.py
+++ exercise-3/palindrome.py
@@ -1,24 +1,15 @@
-# This is the skeleton for exercise-3
-# The goal is to create a crude tokenizer that
-# separates the input string into a list of
-# strings on whitespace.
-
-# In a second step, check if the string is a 
-# palindrome, i. e. whether it reads the same
-# backwards and forwards, ignoring case and
-# whitespace. For this, it might make sense to
-# glue the tokenized list back together directly
-
 def tokenize(input_string):
-	# Change this to return a list
-	return input_string
+  tokens = input_string.split(" ")
+  return tokens
+
 
 def is_palindrome(input_string):
-	tokens = tokenize(input_string)
-	
-	# Change this to return True if the string
-	# is indeed a palindrom and False otherwise
-	return True
+    tokens = tokenize(input_string)
+    reformatted_string = "".join(tokens).lower()
+    if reformatted_string == reformatted_string[::-1]:
+      return True
+    else:
+      return False
 
-# You can use this for fast testing
+
 print(is_palindrome("test"))
