2784ac8a675c0b2165377dd06844217921f4b19d
Scenay <janna@schoenherrs.com>
2021-03-18 19:17:32 +0000

    String wird in liste gesplittet

--- exercise-3/palindrome.py
+++ exercise-3/palindrome.py
@@ -9,16 +9,24 @@
 # whitespace. For this, it might make sense to
 # glue the tokenized list back together directly
 
+input_string = "hello, my name is Peter, I am 26 years old"
+
 def tokenize(input_string):
-	# Change this to return a list
-	return input_string
+  # Change this to return a list
+  return (input_string.split(" "))
+
+for word in tokenize(input_string):
+  return(word.lower())
+
+for word in tokenize(input_string):
+  print(word)
 
 def is_palindrome(input_string):
-	tokens = tokenize(input_string)
-	
-	# Change this to return True if the string
-	# is indeed a palindrom and False otherwise
-	return True
+  tokens = tokenize(input_string)
+
+  # Change this to return True if the string
+  # is indeed a palindrom and False otherwise
+  return True
 
 # You can use this for fast testing
 print(is_palindrome("test"))
