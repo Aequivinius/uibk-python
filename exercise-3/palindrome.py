7a2fba2b87347b82961f8c052b40cdb0951a7220
TeraBT <chris.baumgartner115@gmail.com>
2021-03-18 17:40:00 +0100

    Update palindrome.py

--- exercise-3/palindrome.py
+++ exercise-3/palindrome.py
@@ -9,16 +9,27 @@
 # whitespace. For this, it might make sense to
 # glue the tokenized list back together directly
 
+
 def tokenize(input_string):
-	# Change this to return a list
-	return input_string
+	tokens = input_string.split(" ")
+	
+	return tokens
+
 
 def is_palindrome(input_string):
-	tokens = tokenize(input_string)
+	tokens = "".join(tokenize(input_string)).lower()
+
+	if tokens == tokens[::-1]:
+		return True
+	else:
+		return False
 	
+print(is_palindrome("jajr rjaj"))
+
+
+
 	# Change this to return True if the string
 	# is indeed a palindrom and False otherwise
-	return True
 
 # You can use this for fast testing
-print(is_palindrome("test"))
+##print(is_palindrome("test"))
