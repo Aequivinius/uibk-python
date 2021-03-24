5bbd991071d37ac2798248e0c65e1a7387004701
Vizcachera <milena.peralta-friedburg@uibk.ac.at>
2021-03-24 15:11:22 +0000

    done

--- exercise-3/exercise-3-example.py
+++ exercise-3/exercise-3-example.py
@@ -2,7 +2,7 @@
 # For this demonstration we'll use a short and a
 # long string - the latter we're loading from a
 # file. Don't worry if you don't understand how
-# we're loading the file quite yet. Also, for 
+# we're loading the file quite yet. Also, for
 # demonstration of indexes, a number string
 
 short_string = "Alle meine Entchen"
@@ -17,38 +17,30 @@ print("The length of our strings: ")
 print(len(short_string))
 print(len(long_string))
 print("------------")
-print("-" * 10)
+print("<>" * 10)
 
 # Fun fact: In my python, strings can be
 # multiplied
 
 # Indexing and slicing
-# Since strings can be thought of as lists of 
+# Since strings can be thought of as lists of
 # characters, we can access individual
 # characters using square brackets
-print("First character of short string: " + 
-      short_string[0])
-print("Third character of short string: " +
-      short_string[2])
+print("First character of short string:", short_string[0])
+print("Third character of short string: " + short_string[2])
 long_string_length = len(long_string)
-print("Last character of long string: " +
-      long_string[long_string_length - 1])
-print("Or simpler: " +
-      long_string[-1])
-print("Likewise, the 5th last character: " +
-      long_string[-5])
-print("\n")
+print("Last character of long string: " + long_string[long_string_length - 1])
+print("Or simpler: " + long_string[-1])
+print("Likewise, the 5th last character: " + long_string[-5], "\n")
+#print("\n")
 
 # We can use this notation to extract substrings
-print("2nd up to and including 4th character " +
-      number_string[1:4])
-print("5th last character to 2nd last: " +
-      number_string[-5:-2])
-print("Last 5 characters: "+
-      number_string[-5:])
-print("\n")     
-      
-# Notice the print("\n")? 
+print("2nd up to and including 4th character " + number_string[1:4])
+print("5th last character to 2nd last: " + number_string[-5:-2])
+print("Last 5 characters: " + number_string[-5:])
+print("\n")
+
+# Notice the print("\n")?
 # "\n" is a special character that denotes a new
 # line
 print(short_string + "\n" + short_string)
@@ -66,26 +58,26 @@ lines = long_string.split("\n")
 
 # Let's print the first 5 lines now
 for line in lines[:5]:
-	print(line)
-	
+    print(line)
+
 # Notice the difference if we don't split our
 # string first:
 print("\n")
 print("Using the in operator with a string: ")
 for character in long_string[:5]:
-	print(character)
+    print(character)
 
 # We could also check which of these characters
 # is a white space
 print("Looking for whitespaces: ")
 for character in long_string[:5]:
-	if character == " ":
-		print("character \"" + character + "\" is a whitespace!")
-	elif character =="\n":
-		print("character \"" + character + "\" is a newline!")
-	else:
-		print("character \"" + character + "\" is something else!")
-# notice how we're using \" to print " within a 
+    if character == " ":
+        print("character \"" + character + "\" is a whitespace!")
+    elif character == "\n":
+        print("character \"" + character + "\" is a newline!")
+    else:
+        print("character \"" + character + "\" is something else!")
+# notice how we're using \" to print " within a
 # string - with the backslash we can prevent
 # python from thinking it has reached the end
 # of the string
@@ -93,8 +85,8 @@ for character in long_string[:5]:
 # Also, note how we're introducing 'elif' which
 # stands for 'else if condition', which checks
 # if the condition is true in the case that the
-# if statement before it did apply. 'else' is 
-# used as the 'catch-all' part of the 
+# if statement before it did apply. 'else' is
+# used as the 'catch-all' part of the
 # if-elif-else statement, which applies if none
 # of the conditions before it applied.
 
@@ -103,17 +95,17 @@ for character in long_string[:5]:
 # string - this is a very useful function when
 # dealing with texts!
 for character in long_string[:5]:
-	if character.strip() == "":
-		print("Stripped character is empty")
+    if character.strip() == "":
+        print("Stripped character is empty")
 # "" is called the empty string
 
-# we can also change the capitalisation of 
+# we can also change the capitalisation of
 # strings - you can use .lower() in the same way
 # which is very useful for comparing strings
 print(long_string[:20].upper())
 
 # Finally, we can put lists of strings together
-# again - we could also different strings than 
+# again - we could also different strings than
 # the whitespace, such as the newline, for
 # example, or "[SPACE]"
 print(" ".join(lines[:5]))
--- exercise-3/palindrome.py
+++ exercise-3/palindrome.py
@@ -9,16 +9,28 @@
 # whitespace. For this, it might make sense to
 # glue the tokenized list back together directly
 
+
 def tokenize(input_string):
-	# Change this to return a list
-	return input_string
+  punctuation = "!@#$%^&*()_+<>?:.,;"  #punctuation for comparison and removal
+  for i in input_string: #compares if charachters are punctuation
+    if i in punctuation:
+      input_string = input_string.replace(i, "") # if yes it replaces with empty string = get erraced
+
+  return input_string.split(" ") # splits on the white spaces
+  
 
 def is_palindrome(input_string):
-	tokens = tokenize(input_string)
-	
-	# Change this to return True if the string
-	# is indeed a palindrom and False otherwise
-	return True
+  tokens = tokenize(input_string)
+  new_string = "".join(tokens[:]).lower() # joins string together and makes lower case
+  if new_string == new_string[::-1]: #compares original string with itself itinerated backwards
+    print(input_string + "\n" + new_string, "-> is palindrome!")
+    return True
+  else:
+    print(input_string + "\n" + new_string, " -> not palindrome.")
+    return False
+  	
 
 # You can use this for fast testing
-print(is_palindrome("test"))
+print(is_palindrome("Borrow or rob?"))
+print("\n")
+print(is_palindrome("Winter is coming!"))
