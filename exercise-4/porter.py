0f298b44a4667df9abfc146bf803d91eadc1d6f5
TeraBT <chris.baumgartner115@gmail.com>
2021-04-15 09:32:30 +0200

    Update porter.py

--- exercise-4/porter.py
+++ exercise-4/porter.py
@@ -43,7 +43,7 @@ def ends_in_double_consonant(word):
 
 
 def ends_in_cvc(word):
-	if len(word) > 3:
+	if len(word) >= 3:
 		if is_consonant(word, len(word) - 3) and not is_consonant(word, len(word) - 2) and is_consonant(word, len(word) - 1) and word[-1] not in ['w', 'x', 'y']:
 			return True
 	return False
@@ -66,8 +66,15 @@ def step_1a(word):
 
   # TODO: the rest is up to you!
 
+  elif word[-3:] == "ies":
+    return word[-3:] + "i"
+  elif word[-2:] == "ss":
+    return word[-2] == "ss"
+  elif word[-1:] == "s":
+    return word == word[:-1]
+  else:
   # no rule matches
-  return word
+    return word
 
 def step_1b(word):
 
@@ -86,7 +93,12 @@ def step_1b(word):
 
   # TODO
 
-  return word
+  if ends(word, "ing"):
+    if contains_vowel(word[:-3]):
+      stem = replace(word, "ing", "")
+      return step_1b_helper(stem)
+    else:
+      return word
 
 def step_1b_helper(word):
 
@@ -99,7 +111,7 @@ def step_1b_helper(word):
     if ends(word, suffix):
       return replace(word, suffix, replacement)
 
-  if ends_in_double_consonant(word) and word[len(word) - 1] not in [ "l", "s", "z"]:
+  if ends_in_double_consonant(word) and word[len(word) - 1] not in ["l", "s", "z"]:
     return word[:-1]
 
   if measure(word) == 1 and ends_in_cvc(word):
@@ -109,26 +121,95 @@ def step_1b_helper(word):
 
 def step_1c(word):
   # TODO
+  if contains_vowel(word[:-1]) and word[:-1] == "y":
+    return replace(word, "y", "i")
   return word
 
 def step_2(word):
   # TODO
+  for suffix_pair in [ [ "ational", "ate" ],
+                       [ "tional", "tion" ],
+                       [ "enci", "ence" ],
+                       [ "anci", "ance" ],
+                       [ "izer", "ize" ],
+                       [ "abli", "able" ],
+                       [ "alli", "al" ],
+                       [ "entli", "ent" ],
+                       [ "eli", "e" ],
+                       [ "ousli", "ous" ],
+                       [ "ization", "ize" ],
+                       [ "ation", "ate" ],
+                       [ "ator", "ate" ],
+                       [ "alism", "al" ],
+                       [ "iveness", "ive" ],
+                       [ "fulness", "ful" ],
+                       [ "ousness", "ous" ],
+                       [ "aliti", "al" ],
+                       [ "iviti", "ive" ],
+                       [ "biliti", "ble" ] ]:
+    suffix = suffix_pair[0]
+    replacement = suffix_pair[1]
+    if measure(word) > 0 and ends(word, suffix):
+      return replace(word, suffix, replacement)
   return word
 
 def step_3(word):
   # TODO
+  for suffix_pair in [ [ "icate", "ic" ],
+                       [ "ative", "" ],
+                       [ "alize", "al" ],
+                       [ "iciti", "ic" ],
+                       [ "ical", "ic" ],
+                       [ "ful", "" ],
+                       [ "ness", "" ] ]:
+    suffix = suffix_pair[0]
+    replacement = suffix_pair[1]
+    if measure(word) > 0 and ends(word, suffix):
+      return replace(word, suffix, replacement)
   return word
 
 def step_4(word):
   # TODO
+  for suffix_pair in [ [ "ational", "ate" ],
+                       [ "tional", "tion" ],
+                       [ "enci", "ence" ],
+                       [ "anci", "ance" ],
+                       [ "izer", "ize" ],
+                       [ "abli", "able" ],
+                       [ "alli", "al" ],
+                       [ "entli", "ent" ],
+                       [ "eli", "e" ],
+                       [ "ousli", "ous" ],
+                       [ "ization", "ize" ],
+                       [ "ation", "ate" ],
+                       [ "ator", "ate" ],
+                       [ "alism", "al" ],
+                       [ "iveness", "ive" ],
+                       [ "fulness", "ful" ],
+                       [ "ousness", "ous" ],
+                       [ "aliti", "al" ],
+                       [ "iviti", "ive" ],
+                       [ "biliti", "ble" ] ]:
+    suffix = suffix_pair[0]
+    replacement = suffix_pair[1]
+    if measure(word[:(len(word) - len(suffix))]) > 1 and ends(word, suffix):
+      return replace(word, suffix, replacement)
+    if measure(word[:(len(word) - len(suffix))]) > 1 and word[len(word) - 4] not in ["s", "t"] and ends(word, "ion"):
+      return replace(word, "ion", "")
   return word
 
 def step_5a(word):
   # TODO
+  if measure(word[:-1]) > 1 and ends(word, "e"):
+      return replace(word, "e", "")
+  elif measure(word[:-1]) == 1 and not ends_in_cvc(word[:-1]) and ends(word, "e"):
+    return replace(word, "e", "")
   return word
 
 def step_5b(word):
   # TODO
+  if measure(word) > 1 and ends_in_double_consonant(word) and ends(word, "l"):
+    return replace(word, "l", "")
   return word
 
 def stem(word):
