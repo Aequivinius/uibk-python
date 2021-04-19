05f5db48b3ec8e584c27a244e98e310a0029852e
MairAaron <>
2021-04-19 14:32:57 +0000

    exercise 5

--- exercise-6/bad_importee.py
+++
@@ -1,4 +0,0 @@
-def tokenize(input):
-  return [token.lower().strip(".,!") for token in input.split()]
-
-print(tokenize("Wenn es nur einmal so ganz stille wäre!"))
--- exercise-6/good_importee.py
+++
@@ -1,5 +0,0 @@
-def tokenize(input):
-  return [token.lower().strip(".,!") for token in input.split()]
-
-if __name__ == "__main__":
-  print(tokenize("Wenn das Zufällige und Ungefähre"))
