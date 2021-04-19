f83bf62fe12987fb0efdd7d2da5a8bc8d4f023ea
AlenaPardatsche <>
2021-04-19 13:25:01 +0000

    Exercise5

--- exercise-5/frequencies.py
+++ exercise-5/frequencies.py
@@ -126,7 +126,7 @@ def write_frequencies(frequencies, path):
   return
 
 # TODO: You can comment in the following lines to check your work. When you're finished, it 
-files = traverse_directory('corpus')
-counts = compute_counts(files)
-sorted_counts = sort_counts(counts)
-write_frequencies(sorted_counts, 'frequencies.csv')
+#files = traverse_directory('corpus')
+#counts = compute_counts(files)
+#sorted_counts = sort_counts(counts)
+#write_frequencies(sorted_counts, 'frequencies.csv')
