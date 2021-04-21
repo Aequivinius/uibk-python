440dbfea47bb7752f0710bfa10be498aeada4d6a
Brabl <80413659+Brabl@users.noreply.github.com>
2021-04-21 19:48:20 +0200

    Update frequencies.py

--- exercise-5/frequencies.py
+++ exercise-5/frequencies.py
@@ -134,6 +134,10 @@ def write_frequencies(frequencies, path):
 # TODO: You can comment in the following lines to check
 # your work. When you're finished, it
 #files = traverse_directory('corpus')
+files = traverse_directory('corpus')
 #counts = compute_counts(files)
+counts = compute_counts(files)
 #sorted_counts = sort_counts(counts)
+sorted_counts = sort_counts(counts)
 #write_frequencies(sorted_counts, 'frequencies.csv')
+write_frequencies(sorted_counts, 'frequencies.csv')
