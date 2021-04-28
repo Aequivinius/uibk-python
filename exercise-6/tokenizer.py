bc582551b7f19321f5b42440d34a6cf79a3ad65c
Jo Sch <>
2021-04-28 21:34:05 +0000

    ex6

--- exercise-6/tokenizer.py
+++ exercise-6/tokenizer.py
@@ -5,6 +5,9 @@
 # csv module, which makes writing .csv and .tsv
 # much easier. 
 # TODO: Import them here, one on each line.
+import sys
+import helper
+import csv
 
 def write(input_string, output_path):
   with open(output_path, "w") as f:
@@ -13,7 +16,7 @@ def write(input_string, output_path):
     # it the file as an argument as seen below.
     # (It doesn't work if you haven't imported
     # the csv module above.)
-    writer = csv.writer(f)
+    writer = csv.writer(f, delimiter="\t")
 
     # The csv.writer() method can either just take
     # one argument, the file it will write to, in
@@ -45,7 +48,7 @@ def write(input_string, output_path):
     # using the helper.tokenize() and 
     # helper.normalize() functions, then change
     # the below call to use your list of lists
-    tokens_and_normalizations = [ [] ]
+    tokens_and_normalizations = [[t, helper.normalize(t)] for t in helper.tokenize(input_string)]
     writer.writerows(tokens_and_normalizations)
 
 
@@ -65,11 +68,15 @@ def parse_arguments():
   # TODO: Complete the code below so that
   # input_string contains the string to be tokenized
   # either from file or from user arguments.
-  if sys.argv[1] ...
+  if sys.argv[1] == "-s":
+    input_string = sys.argv[2]
+  elif sys.argv[1] == "-f":
+    with open(sys.argv[2]) as f:
+      input_string = f.read()
 
   # TODO: Change the line below, so that output_file
   # contains the third user-supplied argument
-  outputfile = ""
+  output_file = sys.argv[3]
   return input_string, output_file
 
 
