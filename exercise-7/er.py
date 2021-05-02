18e07dbaba17c8525b7d6c579c2eb2f150056c0e
Karoline Irschara <>
2021-05-02 10:09:15 +0000

    ex_7

--- exercise-7/er.py
+++ exercise-7/er.py
@@ -1,75 +1,88 @@
-
-def find_cs(path, cs,out, ):
-  with open(path ) as f:
-    texts = f.readlines ( )
+def find_cities(path, cities, output):
+  with open(path) as f:
+    texts = f.readlines()
     print(len(texts))
 
-  # texts = texts[:50]
-  with open (out,'w') as g:
-    for t in texts:
-      c = t.split()
-      if c :
-        artid =c[ 0 ]
-        c = [w for w in c \
-            if w[0] not in [ "@", "<" ] ]
+    # texts = texts[:50]
+  with open(output, 'w') as g:
+    for object in texts:
+      split_objects = object.split()
+      if split_objects:
+        article_id = split_objects[0]
+        split_objects = [token for token in split_objects
+                         if token[0] not in ["@", "<"]]
 
         counter = 0
-        for word in c:
-          if word in cs:
-                # find longest match
-                # print("word: " + word)
-            hayst = cs[word]
-                    
+        for word in split_objects:
+          if word in cities:
+            # find longest match
+            # print("word: " + word)
+            haystack = cities[word]
+
             longest = 0
             match = ""
-            for h in hayst:
-              hlen = len(h)
-              if hlen > longest:
-                  tst = " ".join(c[counter:counter+len(h.split())])
+            for hay in haystack:
+              hay_length = len(hay)
+              if hay_length > longest:
+                  longest_match = " ".join(
+                    split_objects[counter:counter+len(hay.split())]
+                    )
 
-              if h == tst:
-                  # print("tst: " + tst)
-                  longest = hlen
-                  match = tst
-                
-                  # print("Found city: " + word)
+              if hay == longest_match:
+                # print("tst: " + tst)
+                  longest = hay_length
+                  match = longest_match
+
+            # print("Found city: " + word)
             if match:
-                   g.write(artid + "," + str(counter) + "," + match + "\n")
+              g.write(article_id + "," + str(counter) + "," + match + "\n")
           counter += 1
 
-  
 
-def ld(path):
-
-    cits = {}
+def make_city_dictionary(path):
+    cities = {}
     with open(path) as f:
-      l = [l.split("\t")[1] for l in f.readlines() ]
-    for cit in l:
-        cs = cit.split()
-        if  cs[0] not in cits:
-          cits[cs[0]] = [ cit.strip() ]
+      line = [line.split("\t")[1] for line in f.readlines()]
+    for city in line:
+        cities_split = city.split()
+        if cities_split[0] not in cities:
+          cities[cities_split[0]] = [city.strip()]
         else:
-          cits[cs[0]].append(cit.strip())
-    
-    for cit in list(cits)[:6]:
-        print(cit + " : " + ",".join(cits[cit]))
-    
+          cities[cities_split[0]].append(city.strip())
+
+    for city in list(cities)[:6]:
+        print(city + " : " + ",".join(cities[city]))
+
     # print(cits["University"])
 
     filters = ["University", "Police", "Of", "Central"]
-    for f in filters :
-        if f in cits:
-            cits[f] = [ c for c in cits[f] if c != f ]
+    for word_form in filters:
+        if word_form in cities:
+          cities[f] = [city for city in cities[word_form]
+                       if city != word_form]
 
-    # print(cits["University"])
+    # print(cities["University"])
 
-    # print(cits)
-    return cits
+    # print(cities)
+    return cities
+
 
 def main(haystack, needles, output):
-  
-  cs = ld(needles)
-  find_cs(haystack, cs, output)
+  """
+    entity recogniser that loads city names from a file and finds occurrences (needles) of them in some free text (haystack)
+
+    Parameters
+    ---------
+    haystack = file to be examined
+    needles = file containing the city names that are looked for
+    output = file where result is stored
+    Returns
+    -------
+    occurrences of city names 
+  """
+  cities = make_city_dictionary(needles)
+  find_cities(haystack, cities, output)
+
 
 if __name__ == "__main__":
   main('text.txt', 'cities15000.txt', 'output.txt')
