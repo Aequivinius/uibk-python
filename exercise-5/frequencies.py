02865de0427f666121f122e9301f84cab189e5c3
JuGrun <>
2021-05-11 09:48:09 +0000

    Ex8

--- exercise-5/frequencies.py
+++ exercise-5/frequencies.py
@@ -7,127 +7,144 @@
 import os
 
 # this function takes a path and returns a list
-# of all the .txt files that lie within it. 
+# of all the .txt files that lie within it.
 def traverse_directory(path):
-  return [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-  
-  # A comprehension! How beautiful! How pythonic!
-  # A longer way to write this follows below.
-  # Note how this part of the code is not executed
-  # because a function stops when it hits a return
-  # statement just like the one above.
-  onlyfiles = []
-  files_and_directories = os.listdir(path) 
-  # Lists all the contents of the path, files and
-  # directories.
-  for f in files_and_directories:
-    # You'd be tempted to call your temporary 
-    # variable 'file', but 'file' is often already
-    # used in python, so it's seldom a good idea
-    fpath = os.path.join(path, f)
-    # Remember how unix-systems use / in paths
-    # and windows uses \? The os.path.join() method
-    # joins together a path to a directory and a
-    # file it contains using the correct separator
-    # depending on the OS you're on. 
+    return [
+        os.path.join(path, f)
+        for f in os.listdir(path)
+        if os.path.isfile(os.path.join(path, f))
+    ]
 
-    # Also, remember that we already know a join()
-    # function - we use it to glue together a list
-    # on a character like 
-    # " ".join(['the', 'cats', 'pyjamas']), which yields
-    # "the cats pyjamas" - you can also join on other 
-    # characters!
-    # This is why we need modules, to differentiate
-    # different methods that have the same name.
-    if os.path.isfile(fpath):
-      # Check if the path points to a file or 
-      # a directory.
-      onlyfiles.add(fpath)
+    # A comprehension! How beautiful! How pythonic!
+    # A longer way to write this follows below.
+    # Note how this part of the code is not executed
+    # because a function stops when it hits a return
+    # statement just like the one above.
+    onlyfiles = []
+    files_and_directories = os.listdir(path)
+    # Lists all the contents of the path, files and
+    # directories.
+    for f in files_and_directories:
+        # You'd be tempted to call your temporary
+        # variable 'file', but 'file' is often already
+        # used in python, so it's seldom a good idea
+        fpath = os.path.join(path, f)
+        # Remember how unix-systems use / in paths
+        # and windows uses \? The os.path.join() method
+        # joins together a path to a directory and a
+        # file it contains using the correct separator
+        # depending on the OS you're on.
+
+        # Also, remember that we already know a join()
+        # function - we use it to glue together a list
+        # on a character like
+        # " ".join(['the', 'cats', 'pyjamas']), which yields
+        # "the cats pyjamas" - you can also join on other
+        # characters!
+        # This is why we need modules, to differentiate
+        # different methods that have the same name.
+        if os.path.isfile(fpath):
+            # Check if the path points to a file or
+            # a directory.
+            onlyfiles.add(fpath)
 
 
 # This function takes a path, opens the file
 # reads its contents and tokenizes them. Returns
 # a list of normalized tokens.
 def tokenize_file(path):
-  # TODO: open and read file contents into the string tokens
-  # TODO: then tokenize and normalize the string:
-  #  1. split on whitespace
-  #  2. lowercase it
-  #  3. strip whitespace
-  #  4. strip (that is, remove at beginning and end) 
-  # special characters such as , . ! ? [ ] ( ) = - ...
-  #  5. check that there are no empty strings in the list
-  f = open(path)
-  content = f.read()
-  tokens = content.split()
-  normalized_tokens = [token.lower().strip(",.!?[]()=-") for token in tokens]
-  for token in normalized_tokens:
-    if len(token) == 0:
-      normalized_tokens.remove(token)
+    # TODO: open and read file contents into the string tokens
+    # TODO: then tokenize and normalize the string:
+    #  1. split on whitespace
+    #  2. lowercase it
+    #  3. strip whitespace
+    #  4. strip (that is, remove at beginning and end)
+    # special characters such as , . ! ? [ ] ( ) = - ...
+    #  5. check that there are no empty strings in the list
+    f = open(path)
+    content = f.read()
+    tokens = content.split()
+    normalized_tokens = [token.lower().strip(",.!?[]()=-") for token in tokens]
+    for token in normalized_tokens:
+        if len(token) == 0:
+            normalized_tokens.remove(token)
 
-  return normalized_tokens
+    return normalized_tokens
+
 
 # this function takes a list of paths, and for every
-# file it calls tokenize_file. Then it populates a 
+# file it calls tokenize_file. Then it populates a
 # dictionary that for every token lists how many
 # times it occurs in the entire corpus, so { word : count }
 def compute_counts(pathlist):
-  counts = {}
-  for path in pathlist:
-    tokens = tokenize_file(path)
-    # TODO: populate the counts dictionary
-    # Check if a token is already in it. If so, add
-    # 1 to its count; if not, create a new entry by using
-    # counts[word] = 1
-    for token in tokens:
-      if token in counts:
-        counts[token] = counts[token] + 1
-      else:
-        counts[token] = 1 
-  
-  return counts
+    counts = {}
+    for path in pathlist:
+        tokens = tokenize_file(path)
+        # TODO: populate the counts dictionary
+        # Check if a token is already in it. If so, add
+        # 1 to its count; if not, create a new entry by using
+        # counts[word] = 1
+        for token in tokens:
+            if token in counts:
+                counts[token] = counts[token] + 1
+            else:
+                counts[token] = 1
+
+    return counts
 
 
-# Dictionaries are great, but they have no order. 
-# { key1 : value1, key2 : value2 } is the same as 
+# Dictionaries are great, but they have no order.
+# { key1 : value1, key2 : value2 } is the same as
 # { key2 : value2, key1 : value1 }
 # So if we want to order a dictionary, we need to resort to
 # lists again, which are ordered (using indexes).
-# This function takes a dictionary as the 
+# This function takes a dictionary as the
 # compute_counts() creates, and returns a list of lists
 # [ [ word1, count1 ], [ word2, count2 ], ... ]
 def sort_counts(counts):
-  sorted_tuples = sorted(counts.items(), key=lambda item: item[1], reverse=True)
-  return sorted_tuples
+    sorted_tuples = sorted(
+        counts.items(), key=lambda item: item[1], reverse=True
+    )
+    return sorted_tuples
 
 
 # this function takes a list of lists as is produced by
 # sort_counts(), opens a new file handle and
 # writes the frequencies in csv format to that file
 def write_frequencies(frequencies, path):
-  # TODO: open the file at path in write mode
-  # then for every item in the list write a new line
-  # the format of the .csv file is as follows:
-  # rank,token,count,frequency
-  
-  # The rank runs starts at 1, so the most frequent word
-  # has rank 1; the second most frequent 2 etc. 
-  # the frequency is calculated by dividing the count by the
-  # sum of all counts. sum([list of numbers]) could be
-  # handy in this case.
-  rank = 0
-  s = 0
-  for t in frequencies:
-    s += t[1]
-  with open(path, 'w') as f:
+    # TODO: open the file at path in write mode
+    # then for every item in the list write a new line
+    # the format of the .csv file is as follows:
+    # rank,token,count,frequency
+
+    # The rank runs starts at 1, so the most frequent word
+    # has rank 1; the second most frequent 2 etc.
+    # the frequency is calculated by dividing the count by the
+    # sum of all counts. sum([list of numbers]) could be
+    # handy in this case.
+    rank = 0
+    s = 0
     for t in frequencies:
-      rank += 1
-      f.write(str(rank) + "," + str(t[0]) + "," + str(t[1]) + "," + str(t[1]/s) + "\n") 
-  return
+        s += t[1]
+    with open(path, "w") as f:
+        for t in frequencies:
+            rank += 1
+            f.write(
+                str(rank)
+                + ","
+                + str(t[0])
+                + ","
+                + str(t[1])
+                + ","
+                + str(t[1] / s)
+                + "\n"
+            )
+    return
+
 
 # TODO: You can comment in the following lines to check
-# your work. When you're finished, it 
-#files = traverse_directory('corpus')
-#counts = compute_counts(files)
-#sorted_counts = sort_counts(counts)
-#write_frequencies(sorted_counts, 'frequencies.csv')
+# your work. When you're finished, it
+# files = traverse_directory('corpus')
+# counts = compute_counts(files)
+# sorted_counts = sort_counts(counts)
+# write_frequencies(sorted_counts, 'frequencies.csv')
--- exercise-7/er.py
+++ exercise-7/er.py
@@ -1,63 +1,76 @@
 def find_cities(path, cities, output):
-  with open(path) as f:
-    texts = f.readlines()
-    print(len(texts))
+    with open(path) as f:
+        texts = f.readlines()
+        print(len(texts))
 
-    # texts = texts[:50]
-  with open(output, 'w') as g:
-    for text in texts:
-      tokens = text.split()
-      if tokens:
-        article_id = tokens[0]
-        tokens = [token for token in tokens if token[0] not in ["@", "<"]]
+        # texts = texts[:50]
+    with open(output, "w") as g:
+        for text in texts:
+            tokens = text.split()
+            if tokens:
+                article_id = tokens[0]
+                tokens = [
+                    token for token in tokens if token[0] not in ["@", "<"]
+                ]
 
-        counter = 0
-        for word in tokens:
-          if word in cities:
-            # find longest match
-            # print("word: " + word)
-            haystack = cities[word]
+                counter = 0
+                for word in tokens:
+                    if word in cities:
+                        # find longest match
+                        # print("word: " + word)
+                        haystack = cities[word]
 
-            longest = 0
-            match = ""
-            for hay in haystack:
-              hay_len = len(hay)
-              if hay_len > longest:
-                longest_match = " ".join(
-                            tokens[counter:counter+len(hay.split())])
+                        longest = 0
+                        match = ""
+                        for hay in haystack:
+                            hay_len = len(hay)
+                            if hay_len > longest:
+                                longest_match = " ".join(
+                                    tokens[
+                                        counter : counter + len(hay.split())
+                                    ]
+                                )
 
-              if hay == longest_match:
-                # print("longest_match: " + longest_match)
-                longest = hay_len
-                match = longest_match
+                            if hay == longest_match:
+                                # print("longest_match: " + longest_match)
+                                longest = hay_len
+                                match = longest_match
 
-                # print("Found city: " + word)
-            if match:
-              g.write(article_id + "," + str(counter) + "," + match + "\n")
-          counter += 1
+                                # print("Found city: " + word)
+                        if match:
+                            g.write(
+                                article_id
+                                + ","
+                                + str(counter)
+                                + ","
+                                + match
+                                + "\n"
+                            )
+                    counter += 1
 
 
 def create_city_dictionary(path):
     cities = {}
     with open(path) as f:
-      line = [line.split("\t")[1] for line in f.readlines()]
+        line = [line.split("\t")[1] for line in f.readlines()]
     for city in line:
-      city_names = city.split()
-      if city_names[0] not in cities:
-        cities[city_names[0]] = [city.strip()]
-      else:
-        cities[city_names[0]].append(city.strip())
+        city_names = city.split()
+        if city_names[0] not in cities:
+            cities[city_names[0]] = [city.strip()]
+        else:
+            cities[city_names[0]].append(city.strip())
 
     for city in list(cities)[:6]:
-      print(city + " : " + ",".join(cities[city]))
+        print(city + " : " + ",".join(cities[city]))
 
     # print(cities["University"])
 
     filters = ["University", "Police", "Of", "Central"]
     for filter_word in filters:
-      if filter_word in cities:
-        cities[filter_word] = [city for city in cities[filter_word]
-                               if city != filter_word]
+        if filter_word in cities:
+            cities[filter_word] = [
+                city for city in cities[filter_word] if city != filter_word
+            ]
 
     # print(cities["University"])
 
@@ -66,22 +79,22 @@ def create_city_dictionary(path):
 
 
 def main(haystack, needles, output):
-  """Finds words in a given text and writes them to a file
+    """Finds words in a given text and writes them to a file
 
-  Parameters
-  ----------
-  haystack the file to search in
-  needles the file with the words to be looked for
-  output the file with the found words
+    Parameters
+    ----------
+    haystack the file to search in
+    needles the file with the words to be looked for
+    output the file with the found words
 
-  Returns
-  -------
-  A file with the found words in it
-  """
+    Returns
+    -------
+    A file with the found words in it
+    """
 
-  cities = create_city_dictionary(needles)
-  find_cities(haystack, cities, output)
+    cities = create_city_dictionary(needles)
+    find_cities(haystack, cities, output)
 
 
 if __name__ == "__main__":
-  main('text.txt', 'cities15000.txt', 'output.txt')
+    main("text.txt", "cities15000.txt", "output.txt")
