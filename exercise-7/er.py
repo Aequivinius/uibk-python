4ddf4d8ca419a61dd30ffc02856cb6fc82e1b99b
Karoline Irschara <>
2021-05-11 10:42:23 +0000

    added the lock file

--- exercise-7/er.py
+++ exercise-7/er.py
@@ -1,54 +1,57 @@
 def find_cities(path, cities, output):
-  with open(path) as f:
-    texts = f.readlines()
-    print(len(texts))
+    with open(path) as f:
+        texts = f.readlines()
+        print(len(texts))
 
-    # texts = texts[:50]
-  with open(output, 'w') as g:
-    for object in texts:
-      split_objects = object.split()
-      if split_objects:
-        article_id = split_objects[0]
-        split_objects = [token for token in split_objects
-                         if token[0] not in ["@", "<"]]
+        # texts = texts[:50]
+    with open(output, "w") as g:
+        for object in texts:
+            split_objects = object.split()
+            if split_objects:
+                article_id = split_objects[0]
+                split_objects = [
+                    token for token in split_objects if token[0] not in ["@", "<"]
+                ]
 
-        counter = 0
-        for word in split_objects:
-          if word in cities:
-            # find longest match
-            # print("word: " + word)
-            haystack = cities[word]
+                counter = 0
+                for word in split_objects:
+                    if word in cities:
+                        # find longest match
+                        # print("word: " + word)
+                        haystack = cities[word]
 
-            longest = 0
-            match = ""
-            for hay in haystack:
-              hay_length = len(hay)
-              if hay_length > longest:
-                  longest_match = " ".join(
-                    split_objects[counter:counter+len(hay.split())]
-                    )
+                        longest = 0
+                        match = ""
+                        for hay in haystack:
+                            hay_length = len(hay)
+                            if hay_length > longest:
+                                longest_match = " ".join(
+                                    split_objects[counter : counter + len(hay.split())]
+                                )
 
-              if hay == longest_match:
-                # print("tst: " + tst)
-                  longest = hay_length
-                  match = longest_match
+                            if hay == longest_match:
+                                # print("tst: " + tst)
+                                longest = hay_length
+                                match = longest_match
 
-            # print("Found city: " + word)
-            if match:
-              g.write(article_id + "," + str(counter) + "," + match + "\n")
-          counter += 1
+                        # print("Found city: " + word)
+                        if match:
+                            g.write(
+                                article_id + "," + str(counter) + "," + match + "\n"
+                            )
+                    counter += 1
 
 
 def make_city_dictionary(path):
     cities = {}
     with open(path) as f:
-      line = [line.split("\t")[1] for line in f.readlines()]
+        line = [line.split("\t")[1] for line in f.readlines()]
     for city in line:
         cities_split = city.split()
         if cities_split[0] not in cities:
-          cities[cities_split[0]] = [city.strip()]
+            cities[cities_split[0]] = [city.strip()]
         else:
-          cities[cities_split[0]].append(city.strip())
+            cities[cities_split[0]].append(city.strip())
 
     for city in list(cities)[:6]:
         print(city + " : " + ",".join(cities[city]))
@@ -58,8 +61,7 @@ def make_city_dictionary(path):
     filters = ["University", "Police", "Of", "Central"]
     for word_form in filters:
         if word_form in cities:
-          cities[f] = [city for city in cities[word_form]
-                       if city != word_form]
+            cities[f] = [city for city in cities[word_form] if city != word_form]
 
     # print(cities["University"])
 
@@ -68,7 +70,7 @@ def make_city_dictionary(path):
 
 
 def main(haystack, needles, output):
-  """
+    """
     entity recogniser that loads city names from a file
     finds occurrences (needles) of them
     in some free text (haystack)
@@ -81,10 +83,10 @@ def main(haystack, needles, output):
     Returns
     -------
     occurrences of city names
-  """
-  cities = make_city_dictionary(needles)
-  find_cities(haystack, cities, output)
+    """
+    cities = make_city_dictionary(needles)
+    find_cities(haystack, cities, output)
 
 
 if __name__ == "__main__":
-  main('text.txt', 'cities15000.txt', 'output.txt')
+    main("text.txt", "cities15000.txt", "output.txt")
