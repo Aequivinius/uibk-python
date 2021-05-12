387dec147d893ebb2c341d35e39ab046d3ff89e3
Vizcachera <>
2021-05-12 12:01:26 +0000

    poetry

--- exercise-4/porter.py
+++ exercise-4/porter.py
@@ -1,241 +1,273 @@
 def tokenize(text):
-  return text.split()
+    return text.split()
+
 
 # call this using is_consonant(word, i)
 
+
 def is_consonant(word, index):
-	if word[index] in ['a', 'e', 'i', 'o', 'u']:
-		return False
-	if word[index] == 'y':
-		if index == 0:
-			return True
-		else:
-			return not is_consonant(word, index - 1)
-	return True
+    if word[index] in ["a", "e", "i", "o", "u"]:
+        return False
+    if word[index] == "y":
+        if index == 0:
+            return True
+        else:
+            return not is_consonant(word, index - 1)
+    return True
 
 
 def is_vowel(word, index):
-	return not is_consonant(word, index)
+    return not is_consonant(word, index)
+
 
 # m = measure of any word or word part when represented in VC
 def measure(word):
-	cvs = ""
-	for i in range(len(word)):
-		if is_consonant(word, i):
-			cvs = cvs + "c"
-		else:
-			cvs = cvs + "v"
+    cvs = ""
+    for i in range(len(word)):
+        if is_consonant(word, i):
+            cvs = cvs + "c"
+        else:
+            cvs = cvs + "v"
 
-	return cvs.count("vc")
+    return cvs.count("vc")
+
 
 # *v* - the stem contains a vowel
 def contains_vowel(word):
-	for index in range(len(word)):
-		if not is_consonant(word, index):
-			return True
-	return False
+    for index in range(len(word)):
+        if not is_consonant(word, index):
+            return True
+    return False
+
 
 # *d  - the stem ends with a double consonant (e.g. -TT, -SS)
 def ends_in_double_consonant(word):
-	if len(word) >= 2 and is_consonant(word, len(word) - 1):
-		if word[-1] == word[-2]:
-			return True
-	return False
+    if len(word) >= 2 and is_consonant(word, len(word) - 1):
+        if word[-1] == word[-2]:
+            return True
+    return False
+
 
 # *o  - the stem ends cvc, where the second c is not W, X or Y (e.g. -WIL, -HOP)
 def ends_in_cvc(word):
-	if len(word) >= 3:
-		if is_consonant(word, len(word) - 3) and not is_consonant(word, len(word) - 2) and is_consonant(word, len(word) - 1) and word[-1] not in ['w', 'x', 'y']:
-			return True
-	return False
+    if len(word) >= 3:
+        if (
+            is_consonant(word, len(word) - 3)
+            and not is_consonant(word, len(word) - 2)
+            and is_consonant(word, len(word) - 1)
+            and word[-1] not in ["w", "x", "y"]
+        ):
+            return True
+    return False
+
 
 def replace(word, suffix, replacement):
-  return word[:-len(suffix)] + replacement
+    return word[: -len(suffix)] + replacement
+
 
 # *S  - the stem ends with S (and similarly for the other letters)
 def ends(word, suffix):
-  return word[-len(suffix):] == suffix
+    return word[-len(suffix) :] == suffix
 
 
 def step_1a(word):
 
-  if word[-4:] == "sses":
-    # remember, Porter algorithm matches
-    # the longest suffix in each step
-    # and then finishes the step
-    # without checking the other rules
-    return word[:-4] + "ss"
+    if word[-4:] == "sses":
+        # remember, Porter algorithm matches
+        # the longest suffix in each step
+        # and then finishes the step
+        # without checking the other rules
+        return word[:-4] + "ss"
 
-  if word[-3:] == "ies":
-    return word[:-3] + "i" 
+    if word[-3:] == "ies":
+        return word[:-3] + "i"
 
-  if word[-2:] == "ss":
-    return word[:-2] + "ss"
+    if word[-2:] == "ss":
+        return word[:-2] + "ss"
 
-  if word[-1:] == "s":
-    return word[:-1]
+    if word[-1:] == "s":
+        return word[:-1]
 
-  # no rule matches
-  return word
+    # no rule matches
+    return word
+
 
 def step_1b(word):
 
-  if ends(word, "eed"):
-    if measure(word[:-3]) > 0:
-      return replace(word, "eed", "ee")
-    else:
-      return word
+    if ends(word, "eed"):
+        if measure(word[:-3]) > 0:
+            return replace(word, "eed", "ee")
+        else:
+            return word
 
-  if ends(word, "ed"):
-    if contains_vowel(word[:-2]):
-      stem = replace(word, "ed", "")
-      return step_1b_helper(stem)
-    else:
-      return word
+    if ends(word, "ed"):
+        if contains_vowel(word[:-2]):
+            stem = replace(word, "ed", "")
+            return step_1b_helper(stem)
+        else:
+            return word
 
-  if ends(word, "ing"):
-    if contains_vowel(word[:-3]):
-      stem1 = replace(word, "ing", "")
-      return step_1b_helper(stem1)
-    else:
-      return word
+    if ends(word, "ing"):
+        if contains_vowel(word[:-3]):
+            stem1 = replace(word, "ing", "")
+            return step_1b_helper(stem1)
+        else:
+            return word
 
-  return word
+    return word
+
 
 def step_1b_helper(word):
 
-  # notice how we're iterating through a list of lists here
-  
+    # notice how we're iterating through a list of lists here
 
-  for suffix_pair in [ [ "at", "ate" ],
-                       [ "bl", "ble" ],
-                       [ "iz", "ize" ] ]:
-    suffix = suffix_pair[0]
-    replacement = suffix_pair[1]
-    if ends(word, suffix):
-      return replace(word, suffix, replacement)
+    for suffix_pair in [["at", "ate"], ["bl", "ble"], ["iz", "ize"]]:
+        suffix = suffix_pair[0]
+        replacement = suffix_pair[1]
+        if ends(word, suffix):
+            return replace(word, suffix, replacement)
 
-  if ends_in_double_consonant(word) and word[len(word) - 1] not in [ "l", "s", "z"]:
-    return word[:-1]
+    if ends_in_double_consonant(word) and word[len(word) - 1] not in ["l", "s", "z"]:
+        return word[:-1]
 
-  if measure(word) == 1 and ends_in_cvc(word):
-    return word + "e"
+    if measure(word) == 1 and ends_in_cvc(word):
+        return word + "e"
 
-  return word
+    return word
+
 
 def step_1c(word):
-  if word[-1] == "y":
-    if contains_vowel(word[:-1]):      
-      return word[:-1] + "i"
-    else:
-      return word
-  return word
-      
-s2_dic = {"ational": "ate",
-          "tional": "tion",
-          "enci": "ence",
-          "anci": "ance",
-          "izer": "ize",
-          "abli": "able",
-          "alli": "al",
-          "entli":"ent",
-          "eli": "e",
-          "ousli": "ous",
-          "ization": "ize",
-          "ation": "ate",
-          "ator": "ate",
-          "alism": "al",
-          "iveness": "ive",
-          "fulness": "ful",
-          "ousness": "ous",
-          "aliti": "al",
-          "iviti": "ive",
-          "biliti": "ble"}
+    if word[-1] == "y":
+        if contains_vowel(word[:-1]):
+            return word[:-1] + "i"
+        else:
+            return word
+    return word
+
+
+s2_dic = {
+    "ational": "ate",
+    "tional": "tion",
+    "enci": "ence",
+    "anci": "ance",
+    "izer": "ize",
+    "abli": "able",
+    "alli": "al",
+    "entli": "ent",
+    "eli": "e",
+    "ousli": "ous",
+    "ization": "ize",
+    "ation": "ate",
+    "ator": "ate",
+    "alism": "al",
+    "iveness": "ive",
+    "fulness": "ful",
+    "ousness": "ous",
+    "aliti": "al",
+    "iviti": "ive",
+    "biliti": "ble",
+}
+
 
 def step_2(word):
-  if len(word) > 3 and word[-2] in "acelost":
-    for suffix, ending in s2_dic.items():
-      if ends(word, str(suffix)) and (measure(word[:-len(suffix)]) > 0):
-        return replace(word, str(suffix), str(ending))
-      
-  return word
+    if len(word) > 3 and word[-2] in "acelost":
+        for suffix, ending in s2_dic.items():
+            if ends(word, str(suffix)) and (measure(word[: -len(suffix)]) > 0):
+                return replace(word, str(suffix), str(ending))
 
-s3_dic = {"icate": "ic",
-          "ative": "",
-          "aticate": "",
-          "alize": "al",
-          "iciti": "ic",
-          "ical": "ic",
-          "ful": "",
-          "ness": ""}
+    return word
+
+
+s3_dic = {
+    "icate": "ic",
+    "ative": "",
+    "aticate": "",
+    "alize": "al",
+    "iciti": "ic",
+    "ical": "ic",
+    "ful": "",
+    "ness": "",
+}
+
 
 def step_3(word):
-  if len(word) > 3:
-    for suffix, ending in s3_dic.items():
-      if ends(word, str(suffix)) and (measure(word[:-len(suffix)]) > 0):
-        return replace(word, str(suffix), str(ending))
-  
-  return word
+    if len(word) > 3:
+        for suffix, ending in s3_dic.items():
+            if ends(word, str(suffix)) and (measure(word[: -len(suffix)]) > 0):
+                return replace(word, str(suffix), str(ending))
+
+    return word
 
 
-s4_dic = {"al":"",
-          "ance":"",
-          "ence":"",
-          "er":"",
-          "ic":"",
-          "able":"",
-          "ible":"",
-          "ant":"",
-          "ement":"",
-          "ment":"",
-          "ent":"",
-          "ou":"",
-          "ism":"",
-          "ate":"",
-          "iti":"",
-          "ous":"",
-          "ive":"",
-          "ize":""}
+s4_dic = {
+    "al": "",
+    "ance": "",
+    "ence": "",
+    "er": "",
+    "ic": "",
+    "able": "",
+    "ible": "",
+    "ant": "",
+    "ement": "",
+    "ment": "",
+    "ent": "",
+    "ou": "",
+    "ism": "",
+    "ate": "",
+    "iti": "",
+    "ous": "",
+    "ive": "",
+    "ize": "",
+}
+
 
 def step_4(word):
-  if ends(word, "sion") or ends(word, "tion") and (measure(word[:-4]) > 1):
-    return replace(word, "ion", "")
+    if ends(word, "sion") or ends(word, "tion") and (measure(word[:-4]) > 1):
+        return replace(word, "ion", "")
 
-  for suffix, ending in s4_dic.items():
-    if ends(word, str(suffix)) and measure(word[:-len(suffix)]) > 1:
-      return replace(word, str(suffix), str(ending))
+    for suffix, ending in s4_dic.items():
+        if ends(word, str(suffix)) and measure(word[: -len(suffix)]) > 1:
+            return replace(word, str(suffix), str(ending))
 
-  
-  
-  return word
+    return word
+
 
 def step_5a(word):
-  if word[-1] == "e":
-    if measure(word[:-1]) == 1 and not ends_in_cvc(word[:-1]):
-      return word[:-1]
-    if measure(word[:-1]) > 1:
-      return word[:-1]
-    else: return word    
-  
-  return word
+    if word[-1] == "e":
+        if measure(word[:-1]) == 1 and not ends_in_cvc(word[:-1]):
+            return word[:-1]
+        if measure(word[:-1]) > 1:
+            return word[:-1]
+        else:
+            return word
+
+    return word
+
 
 def step_5b(word):
-  if measure(word) > 1 and ends_in_double_consonant(word):
-    return word[:-1]
+    if measure(word) > 1 and ends_in_double_consonant(word):
+        return word[:-1]
 
-  return word
+    return word
+
 
 def stem(word):
-  stem = step_1a(word)
-  stem = step_1b(stem)
-  stem = step_1c(stem)
-  stem = step_2(stem)
-  stem = step_3(stem)
-  stem = step_4(stem)
-  stem = step_5a(stem)
-  stem = step_5b(stem)
+    stem = step_1a(word)
+    stem = step_1b(stem)
+    stem = step_1c(stem)
+    stem = step_2(stem)
+    stem = step_3(stem)
+    stem = step_4(stem)
+    stem = step_5a(stem)
+    stem = step_5b(stem)
 
-  return stem
+    return stem
 
-result = [ { word : stem(word) } for word in tokenize("I agreed with the greatest minds of my generalization destroyed by caresses")]
+
+result = [
+    {word: stem(word)}
+    for word in tokenize(
+        "I agreed with the greatest minds of my generalization destroyed by caresses"
+    )
+]
 print(result)
--- exercise-5/frequencies.py
+++ exercise-5/frequencies.py
@@ -7,140 +7,159 @@
 import os
 
 # this function takes a path and returns a list
-# of all the .txt files that lie within it. 
+# of all the .txt files that lie within it.
 def traverse_directory(path):
-  return [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-  
-  # A comprehension! How beautiful! How pythonic!
-  # A longer way to write this follows below.
-  # Note how this part of the code is not executed
-  # because a function stops when it hits a return
-  # statement just like the one above.
-  onlyfiles = []
-  files_and_directories = os.listdir(path) 
-  # Lists all the contents of the path, files and
-  # directories.
-  for f in files_and_directories:
-    # You'd be tempted to call your temporary 
-    # variable 'file', but 'file' is often already
-    # used in python, so it's seldom a good idea
-    fpath = os.path.join(path, f)
-    # Remember how unix-systems use / in paths
-    # and windows uses \? The os.path.join() method
-    # joins together a path to a directory and a
-    # file it contains using the correct separator
-    # depending on the OS you're on. 
+    return [
+        os.path.join(path, f)
+        for f in os.listdir(path)
+        if os.path.isfile(os.path.join(path, f))
+    ]
 
-    # Also, remember that we already know a join()
-    # function - we use it to glue together a list
-    # on a character like 
-    # " ".join(['the', 'cats', 'pyjamas']), which yields
-    # "the cats pyjamas" - you can also join on other 
-    # characters!
-    # This is why we need modules, to differentiate
-    # different methods that have the same name.
-    if os.path.isfile(fpath):
-      # Check if the path points to a file or 
-      # a directory.
-      onlyfiles.add(fpath)
+    # A comprehension! How beautiful! How pythonic!
+    # A longer way to write this follows below.
+    # Note how this part of the code is not executed
+    # because a function stops when it hits a return
+    # statement just like the one above.
+    onlyfiles = []
+    files_and_directories = os.listdir(path)
+    # Lists all the contents of the path, files and
+    # directories.
+    for f in files_and_directories:
+        # You'd be tempted to call your temporary
+        # variable 'file', but 'file' is often already
+        # used in python, so it's seldom a good idea
+        fpath = os.path.join(path, f)
+        # Remember how unix-systems use / in paths
+        # and windows uses \? The os.path.join() method
+        # joins together a path to a directory and a
+        # file it contains using the correct separator
+        # depending on the OS you're on.
+
+        # Also, remember that we already know a join()
+        # function - we use it to glue together a list
+        # on a character like
+        # " ".join(['the', 'cats', 'pyjamas']), which yields
+        # "the cats pyjamas" - you can also join on other
+        # characters!
+        # This is why we need modules, to differentiate
+        # different methods that have the same name.
+        if os.path.isfile(fpath):
+            # Check if the path points to a file or
+            # a directory.
+            onlyfiles.add(fpath)
 
 
 # This function takes a path, opens the file
 # reads its contents and tokenizes them. Returns
 # a list of normalized tokens.
 def tokenize_file(path):
-  # TODO: open and read file contents into the string tokens
-  f = open(path)
-  tokens = f.read()
+    # TODO: open and read file contents into the string tokens
+    f = open(path)
+    tokens = f.read()
 
-  # TODO: tokenize and normalize the string:
-  # special characters such as , . ! ? [ ] ( ) = - ...
-  punctuation = "!@#$%^&*()_+<>?:.,;?-[]=/'"  #punctuation for comparison and removal
-  for i in tokens: #compares if charachters are punctuation
-    if i in punctuation:
-      tokens = tokens.replace(i, "") # if yes it replaces with empty string = get erraced
-    if i =='\n':
-      tokens = tokens.replace(i, " ")
+    # TODO: tokenize and normalize the string:
+    # special characters such as , . ! ? [ ] ( ) = - ...
+    punctuation = "!@#$%^&*()_+<>?:.,;?-[]=/'"  # punctuation for comparison and removal
+    for i in tokens:  # compares if charachters are punctuation
+        if i in punctuation:
+            tokens = tokens.replace(
+                i, ""
+            )  # if yes it replaces with empty string = get erraced
+        if i == "\n":
+            tokens = tokens.replace(i, " ")
 
-  # lowercase it and strip (that is, remove at beginning and end)
-  tokens = tokens.lower().strip()
+    # lowercase it and strip (that is, remove at beginning and end)
+    tokens = tokens.lower().strip()
 
-  # split on whitespace
-  tokens = tokens.split(" ")  
-  #print(type(tokens))
-  # strip whitespace
-  
-  #  5. check that there are no empty strings in the list  
-  normalized_tokens = [token for token in tokens if token != ""]
-  #print(normalized_tokens)
-  return normalized_tokens
+    # split on whitespace
+    tokens = tokens.split(" ")
+    # print(type(tokens))
+    # strip whitespace
+
+    #  5. check that there are no empty strings in the list
+    normalized_tokens = [token for token in tokens if token != ""]
+    # print(normalized_tokens)
+    return normalized_tokens
+
 
 # this function takes a list of paths, and for every
-# file it calls tokenize_file. Then it populates a 
+# file it calls tokenize_file. Then it populates a
 # dictionary that for every token lists how many
 # times it occurs in the entire corpus, so { word : count }
 def compute_counts(pathlist):
-  counts = {}
-  for path in pathlist:
-    tokens = tokenize_file(path)
-    for token in tokens:
-      if token in counts:
-        counts[token] += 1
-      else:
-        counts[token] = 1
-    # TODO: populate the counts dictionary
-    # Check if a token is already in it. If so, add
-    # 1 to its count; if not, create a new entry by using
-    # counts[word] = 1
-  #print(counts)
-  return counts
+    counts = {}
+    for path in pathlist:
+        tokens = tokenize_file(path)
+        for token in tokens:
+            if token in counts:
+                counts[token] += 1
+            else:
+                counts[token] = 1
+        # TODO: populate the counts dictionary
+        # Check if a token is already in it. If so, add
+        # 1 to its count; if not, create a new entry by using
+        # counts[word] = 1
+    # print(counts)
+    return counts
 
 
-# Dictionaries are great, but they have no order. 
-# { key1 : value1, key2 : value2 } is the same as 
+# Dictionaries are great, but they have no order.
+# { key1 : value1, key2 : value2 } is the same as
 # { key2 : value2, key1 : value1 }
 # So if we want to order a dictionary, we need to resort to
 # lists again, which are ordered (using indexes).
-# This function takes a dictionary as the 
+# This function takes a dictionary as the
 # compute_counts() creates, and returns a list of lists
 # [ [ word1, count1 ], [ word2, count2 ], ... ]
 def sort_counts(counts):
-  sorted_tuples = sorted(counts.items(), key=lambda item: item[1], reverse=True)
-  #print(sorted_tuples)
-  return sorted_tuples
+    sorted_tuples = sorted(counts.items(), key=lambda item: item[1], reverse=True)
+    # print(sorted_tuples)
+    return sorted_tuples
 
 
 # this function takes a list of lists as is produced by
 # sort_counts(), opens a new file handle and
 # writes the frequencies in csv format to that file
 def write_frequencies(frequencies, path):
-  # TODO: open the file at path in write mode
-  with open(path, 'w') as f:
-    # then for every item in the list write a new line
-    #[f.write(str(token).replace("(", "").replace(")", "") + '\n') for token in frequencies]
-    rank = 0
-    for token in frequencies:
-      f.write(str(rank+1)+ "," + str(token).replace("(", "").replace(")", "").replace("[", "").replace("]", "").replace("'", "").replace(" ", "") + "," + str(round(token[1]/sum(c[1] for c in frequencies), 3)) + '\n')
-      rank += 1
-    
+    # TODO: open the file at path in write mode
+    with open(path, "w") as f:
+        # then for every item in the list write a new line
+        # [f.write(str(token).replace("(", "").replace(")", "") + '\n') for token in frequencies]
+        rank = 0
+        for token in frequencies:
+            f.write(
+                str(rank + 1)
+                + ","
+                + str(token)
+                .replace("(", "")
+                .replace(")", "")
+                .replace("[", "")
+                .replace("]", "")
+                .replace("'", "")
+                .replace(" ", "")
+                + ","
+                + str(round(token[1] / sum(c[1] for c in frequencies), 3))
+                + "\n"
+            )
+            rank += 1
 
-  
-  # the format of the .csv file is as follows:
-  # rank,token,count,frequency
-  
-  # The rank runs starts at 1, so the most frequent word
-  # has rank 1; the second most frequent 2 etc. 
-  # the frequency is calculated by dividing the count by the
-  # sum of all counts. sum([list of numbers]) could be
-  # handy in this case.
-  return
+    # the format of the .csv file is as follows:
+    # rank,token,count,frequency
+
+    # The rank runs starts at 1, so the most frequent word
+    # has rank 1; the second most frequent 2 etc.
+    # the frequency is calculated by dividing the count by the
+    # sum of all counts. sum([list of numbers]) could be
+    # handy in this case.
+    return
+
 
 # TODO: You can comment in the following lines to check
-# your work. When you're finished, it 
-files = traverse_directory('corpus')
+# your work. When you're finished, it
+files = traverse_directory("corpus")
 counts = compute_counts(files)
 sorted_counts = sort_counts(counts)
-write_frequencies(sorted_counts, 'frequencies.csv')
-#tokenize_file('demo.txt')
-#sort_counts(compute_counts(['demo.txt', 'demo1.txt']))
-#write_frequencies(sort_counts(compute_counts(['demo.txt', 'demo1.txt'])), 'frequencies.csv')
+write_frequencies(sorted_counts, "frequencies.csv")
+# tokenize_file('demo.txt')
+# sort_counts(compute_counts(['demo.txt', 'demo1.txt']))
+# write_frequencies(sort_counts(compute_counts(['demo.txt', 'demo1.txt'])), 'frequencies.csv')
